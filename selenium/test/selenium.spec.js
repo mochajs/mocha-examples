8b85e18435060ccbfea4459e5342cdc0983fbb5b
Craig Taub <craigtaub@gmail.com>
2019-04-20 12:11:25 +0100

    cleanup selenium

--- selenium/test/selenium.spec.js
+++ selenium/test/selenium.spec.js
@@ -7,16 +7,17 @@ let driver = null;
 const chromeOptions = new chrome.Options().headless();
 
 describe("Selenium", () => {
+
   beforeEach(async () => {
-    driver = new Builder()
-    .forBrowser('chrome')
-    .setChromeOptions(chromeOptions)
-    .build();
+    driver = new Builder().forBrowser('chrome').setChromeOptions(chromeOptions).build();
+
     await driver.get("http://www.google.com/");
   });
+
   afterEach(async () => {
     await driver.quit();
   });
+
   it("should render \"Selenium WebDriver\" on a Google search result", async () => {
     const element = await driver.findElement(By.name("q"));
     await element.sendKeys("webdriver", Key.RETURN);
@@ -24,4 +25,5 @@ describe("Selenium", () => {
 
     assert.equal(res, "Selenium WebDriver");
   });
+ 
 });

c2c01318fe853991d546719db2d7a3c21c662020
Seohyun Yoon <sarahseohyun@gmail.com>
2019-10-10 02:12:04 +0900

    add tslint config

--- typescript-babel/src/index.ts
+++ typescript-babel/src/index.ts
@@ -1,1 +1,1 @@
-export { default as hello } from './hello'
+export { default as hello } from "./hello";
--- typescript-babel/test/index.spec.ts
+++ typescript-babel/test/index.spec.ts
@@ -1,11 +1,8 @@
-import { equal } from 'assert'
-import { hello } from '../src'
+import { equal } from "assert";
+import { hello } from "../src";
 
-describe('Typescript + Babel usage suite', () => {
-  it('should be able to execute a test', () => {
-    equal(true, true)
-  })
-  it('should return string correctly', () => {
-    equal(hello('mocha'), 'Hello mocha')
-  })
-})
+describe("Typescript + Babel usage suite", () => {
+  it("should return string correctly", () => {
+    equal(hello("mocha"), "Hello mocha");
+  });
+});

74ee0aa0c4ef16218ac5d8a4eb02602314bbc922
Eunji Kim <eunk.angie@outlook.com>
2019-10-13 21:20:34 +0900

    fix npm start for shebang, port assign to 3000

--- express-rest-api/server/index.js
+++ express-rest-api/server/index.js
@@ -3,6 +3,9 @@
 const app = require('./app');
 const debug = require('debug')('example:server');
 
+const port = parseInt(process.env.PORT || '3000', 10);
+app.set('port', port);
+
 app.listen(app.get('port'), function () {
-  debug('Express server listening on port ' + app.get('port'));
+  debug(`Express server listening on port ${app.get('port')}`);
 });

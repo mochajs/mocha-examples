6bd748e73576672568ab519a6a75d3ab5ff3b33c
irrationnelle <drakkarverenis@gmail.com>
2020-10-18 01:27:57 +0900

    fix: reformat with prettier

--- packages/rxjs/custom-operator.js
+++ packages/rxjs/custom-operator.js
@@ -1,10 +1,10 @@
-const { map, throttleTime } = require('rxjs/operators');
+const { map, throttleTime } = require("rxjs/operators");
 
-const getNames = (source) => source.pipe(map(value => value.name));
+const getNames = (source) => source.pipe(map((value) => value.name));
 
 const throttleTimeForThreeMs = (source) => source.pipe(throttleTime(3));
 
 module.exports = {
   getNames,
-  throttleTimeForThreeMs
-}
+  throttleTimeForThreeMs,
+};
--- packages/rxjs/test/index.test.js
+++ packages/rxjs/test/index.test.js
@@ -1,68 +1,55 @@
-const { TestScheduler } = require('rxjs/testing');
-const { from, of } = require('rxjs');
-const { delay, concatMap } = require('rxjs/operators')
-const { deepEqual } = require('assert');
+const { TestScheduler } = require("rxjs/testing");
+const { from, of } = require("rxjs");
+const { delay, concatMap } = require("rxjs/operators");
+const { deepEqual } = require("assert");
 
-const { getNames, throttleTimeForThreeMs } = require('../custom-operator');
+const { getNames, throttleTimeForThreeMs } = require("../custom-operator");
 
-describe('observable stream', ()=> {
+describe("observable stream", () => {
   let testScheduler;
   const sourceValues = [
     {
-      name: 'mocha'
-    }
-    ,{
-      name: 'vanilla'
-    }
-    ,{
-      name: 'caramel'
-    }
-  ]
+      name: "mocha",
+    },
+    {
+      name: "vanilla",
+    },
+    {
+      name: "caramel",
+    },
+  ];
 
-  beforeEach(()=> {
+  beforeEach(() => {
     testScheduler = new TestScheduler((actual, expected) => {
       deepEqual(actual, expected);
     });
-  })
+  });
 
-  it('should follow marble diagrams with basic operators', ()=> {
+  it("should follow marble diagrams with basic operators", () => {
     testScheduler.run(({ cold, expectObservable, expectSubscriptions }) => {
-      const targetStream =  cold('01(2|)', sourceValues);
-      const subs =               '^-!';
-      const expected =           '--0-1-(2|)';
+      const targetStream = cold("01(2|)", sourceValues);
+      const subs = "^-!";
+      const expected = "--0-1-(2|)";
 
       expectObservable(
-        targetStream.pipe(
-          concatMap(
-            value => of(value).pipe(
-              delay(2)
-            )
-          )
-        )
+        targetStream.pipe(concatMap((value) => of(value).pipe(delay(2))))
       ).toBe(expected, sourceValues);
-      expectSubscriptions(targetStream.subscriptions).toBe(subs);})
-  })
+      expectSubscriptions(targetStream.subscriptions).toBe(subs);
+    });
+  });
 
-  it('should return computed values with custom operators, following the marble diagram', () => {
+  it("should return computed values with custom operators, following the marble diagram", () => {
     testScheduler.run(({ expectObservable }) => {
       const targetStream = from(sourceValues).pipe(
-        concatMap(value => 
-          of(value).pipe(
-            delay(2)
-          )
-        )
-      )
-      const expected = '--0---(1|)';
+        concatMap((value) => of(value).pipe(delay(2)))
+      );
+      const expected = "--0---(1|)";
 
-      const resultValues = ['mocha','caramel'];
+      const resultValues = ["mocha", "caramel"];
 
       expectObservable(
-        targetStream.pipe(
-          getNames, 
-          throttleTimeForThreeMs
-        )
+        targetStream.pipe(getNames, throttleTimeForThreeMs)
       ).toBe(expected, resultValues);
     });
   });
-})
-
+});

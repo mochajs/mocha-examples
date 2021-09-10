11261222dbb9a27f93cc216b2dbde06fa8c7a75a
Juerg B <44573692+juergba@users.noreply.github.com>
2021-09-10 17:04:20 +0200

    replace '--delay' test example
    sample for dynamically generating tests

--- packages/async-setup-with-delay/test/delay.spec.js
+++ packages/async-setup-with-delay/test/delay.spec.js
@@ -1,25 +1,23 @@
 'use strict';
 const assert = require('assert');
-const delay = 3000;
 
-// Just for comparison
-describe('mocha-test 1', () => {
-  console.log('Started mocha-test 1 ...');
-  it('will be started immediately but finished with delay', () => {
-    assert.ok(true);
-    console.log('... Finished mocha-test 1');
+(async function() {
+  const tests = await new Promise(resolve => {
+    setTimeout(resolve, 5000, [
+        {args: [1, 2], expected: 3},
+        {args: [1, 2, 3], expected: 6},
+        {args: [1, 2, 3, 4], expected: 10}
+    ]);
   });
-});
 
-// Some tests that require delay
-setTimeout(function() {
-  describe('mocha-test 2 (with delay)', function() {
-    // Will be started with delay
-    console.log('Started mocha-test 2 ...');
-    it('will be started with delay and finished', () => {
-      assert.ok(true);
-      console.log('... Finished mocha-test 2');
+  describe('add()', function() {
+    tests.forEach(({args, expected}) => {
+      it(`correctly adds ${args.length} args`, function() {
+        const res = args.reduce((sum, curr) => sum + curr, 0);
+        assert.strictEqual(res, expected);
+      });
     });
-    run();
   });
-}, delay);
+
+  run();
+})();

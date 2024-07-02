174bbcc6ead8a0feae784e9a70dc4b74265e0964
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2024-07-01 21:43:44 -0400

    feat: added hello world example (#73)

---
+++ packages/hello-world/README.md
@@ -0,0 +1,6 @@
+# Hello World Example
+
+A straightforward example of using Mocha with no additional options.
+
+1. `npm install`
+2. `npm run test`
---
+++ packages/hello-world/test.spec.js
@@ -0,0 +1,7 @@
+const assert = require("assert");
+
+describe("example", () => {
+    it("2 + 2", () => {
+        assert.equal(2 + 2, 4);
+    });
+});

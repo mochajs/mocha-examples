3805ea01d9f23e6663a0cfb8504a4f350c35d0cf
Josh Goldberg <git@joshuakgoldberg.com>
2025-02-16 11:10:55 -0500

    Attempt: headless, no-sandbox

--- packages/puppeteer/test/index.spec.js
+++ packages/puppeteer/test/index.spec.js
@@ -1,35 +1,42 @@
-const assert = require('assert');
-const puppeteer = require('puppeteer'); 
-const opts = process.env.D ? { headless: false, slowMo: 250 } : {}; 
+const assert = require("assert");
+const puppeteer = require("puppeteer");
+const opts = process.env.D
+  ? { headless: false, slowMo: 250 }
+  : process.env.CI
+  ? {
+      headless: true,
+      args: [`--no-sandbox`, `--disable-setuid-sandbox`],
+    }
+  : {};
 
-describe('my app', function() {
+describe("my app", function () {
   let browser;
   let page;
   let server;
 
-  before(async function() { 
+  before(async function () {
     this.timeout(10000);
     // Create an Express static server that will serve up `index.html` at
     // `http://localhost:3000/index.html`
-    const app = require('express')();
-    app.use(require('express-static')('.'));
+    const app = require("express")();
+    app.use(require("express-static")("."));
     server = await app.listen(3001);
 
     // Launch Puppeteer and navigate to the Express server
     browser = await puppeteer.launch(opts);
     page = await browser.newPage();
-    await page.goto('http://localhost:3001/index.html'); 
+    await page.goto("http://localhost:3001/index.html");
   });
 
-  after(async function() {
+  after(async function () {
     await browser.close();
     await server.close();
   });
 
-  it('h1 should say "mocha is good"', async function() {  
-    const tag = 'h1'; 
+  it('h1 should say "mocha is good"', async function () {
+    const tag = "h1";
     await page.waitForSelector(tag);
-    const heading = await page.$eval(tag, heading => heading.innerText);
-    assert.equal(heading, 'Mocha is good') 
-  }); 
+    const heading = await page.$eval(tag, (heading) => heading.innerText);
+    assert.equal(heading, "Mocha is good");
+  });
 });

b59120128d0a419b6fe5446a0d9535dc8c9f8338
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2025-02-16 10:53:45 -0500

    fix(react-webpack): use .jsx extension for JSX test (#108)
    * fix(react-webpack): use .jsx extension for JSX test
    
    * Fully switches to .jsx files for JSX syntax

--- packages/react-webpack/.mocharc.js
+++ packages/react-webpack/.mocharc.js
@@ -1,4 +1,4 @@
 module.exports = {
   require: '@babel/register',
-  extension: ['js']
+  extension: ['js', 'jsx']
 };
--- packages/react-webpack/webpack.config.js
+++ packages/react-webpack/webpack.config.js
@@ -4,12 +4,12 @@ const path = require('path');
 Below we configure webpack to build a client-side bundle with the following instruction:
  - use the "babel-loader" module for preprocessing of ES6
  - in "development" mode so webpack will not use any built-in optimisations
- - start processing code from file "./src/index.js"
+ - start processing code from file "./src/index.jsx"
  - output bundle to "lib/index_bundle.js"
 */
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.jsx',
   output: {
     path: path.resolve('lib'),
     filename: 'index_bundle.js'
@@ -18,7 +18,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.m?js$/,
+        test: /\.m?jsx?$/,
         exclude: /(node_modules)/,
         use: {
           loader: 'babel-loader',
